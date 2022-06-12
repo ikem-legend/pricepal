34e959a7023352868a81fe1da1b29d4556e9e0b5
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-06-12 15:52:18 +0100

    Trying to fix update bug

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsOptional } from 'class-validator';
+import { Exclude } from 'class-transformer';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @Exclude()
+  createdAt: Date;
+
+  @IsOptional()
+  @Exclude()
+  updatedAt: Date;
+}
