2af0fc647ff17e820fca7bf329fd1de18fee4277
Ikemefuna Obioha <ikemoobioha@gmail.com>
2022-06-12 19:28:19 +0100

    Fixed create and update bug

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,14 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsOptional } from 'class-validator';
-import { Exclude } from 'class-transformer';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsOptional()
-  @Exclude()
-  createdAt: Date;
-
-  @IsOptional()
-  @Exclude()
-  updatedAt: Date;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
